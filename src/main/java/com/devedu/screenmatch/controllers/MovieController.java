d0d215f3faf5071a74129ca4b285b963c104cb36
Eduardo <eduguimara@gmail.com>
2023-07-10 20:57:02 -0300

    Movies List implemented

--- src/main/java/com/devedu/screenmatch/controllers/MovieController.java
+++ src/main/java/com/devedu/screenmatch/controllers/MovieController.java
@@ -6,6 +6,7 @@ import com.devedu.screenmatch.repositories.MovieRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,16 +24,17 @@ public class MovieController {
     }
 
     @GetMapping
-    public String openListPage(){
+    public String openListPage(Model model){
+        model.addAttribute("movies", movieRepository.getAllMovies());
         return "movies/list";
     }
 
-    @PostMapping("/form")
+    @PostMapping
     public String registerMovie(MovieDTO movieDTO) {
         var movie = new Movie();
         BeanUtils.copyProperties(movieDTO, movie);
         movieRepository.addMovie(movie);
         System.out.println(movieRepository.getAllMovies());
-        return "movies/form";
+        return "redirect:/movies";
     }
 }
