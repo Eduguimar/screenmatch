3c1a069d554d78d22a02c02c30075ca41f5dbeb3
Eduardo <eduguimara@gmail.com>
2023-07-22 20:05:17 -0300

    Delete movie created

--- src/main/java/com/devedu/screenmatch/controllers/MovieController.java
+++ src/main/java/com/devedu/screenmatch/controllers/MovieController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +35,12 @@ public class MovieController {
         var movie = new Movie();
         BeanUtils.copyProperties(movieDTO, movie);
         movieRepository.save(movie);
+        return "redirect:/movies";
+    }
+
+    @DeleteMapping
+    public String deleteMovie(Long id) {
+        movieRepository.deleteById(id);
         return "redirect:/movies";
     }
 }
